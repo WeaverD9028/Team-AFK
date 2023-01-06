873a24d584128c9fd61680f6da48e905695b0d5b
DanntheKING <Danielwinston669485@gmail.com>
2023-01-06 16:28:50 -0600

    AutoAid
    AutoAid Template

---
+++ app/src/main/java/com/example/autoaid/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.autoaid
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/com/example/autoaid/ReportActivity.kt
@@ -0,0 +1,4 @@
+package com.example.autoaid
+
+class ReportActivity {
+}
