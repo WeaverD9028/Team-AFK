b9a7adf2a3897b02dca9a8c46332b2aa35ae8b9a
Flavall19 <77989246+Flavall19@users.noreply.github.com>
2023-01-19 17:15:19 -0600

    Car Selection Page

--- app/src/main/java/com/example/autoaid/MenuActivity.kt
+++ app/src/main/java/com/example/autoaid/MenuActivity.kt
@@ -2,12 +2,23 @@ package com.example.autoaid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        button2.setOnClickListener {
+            Intent(this, SymptomsActivity::class.java).also {
+                startActivity(it)
+
+            }
+        }
+
     }
 
 }
