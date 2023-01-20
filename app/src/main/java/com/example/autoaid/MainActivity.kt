b3614b40b92749f202a45eaf33507115a654d058
WeaverD9028 <dcw025@latech.edu>
2023-01-19 20:45:58 -0600

    Changes to my branch

--- app/src/main/java/com/example/autoaid/MainActivity.kt
+++ app/src/main/java/com/example/autoaid/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import com.app.dylanw_autoaid.ActivitySymptomsBinding
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 
 }
