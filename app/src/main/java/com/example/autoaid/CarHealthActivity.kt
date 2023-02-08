31eddced6c3760960802173510a0485f8176bfe6
Amiyah <51186075+amiyahfrierson@users.noreply.github.com>
2023-02-07 21:36:20 -0600

    final changes

--- app/src/main/java/com/example/autoaid/CarHealthActivity.kt
+++ app/src/main/java/com/example/autoaid/CarHealthActivity.kt
@@ -7,5 +7,6 @@ class CarHealthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_health)
+
     }
 }
