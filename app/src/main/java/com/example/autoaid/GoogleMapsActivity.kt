49c9c9e4753962817830fa23d65f709fd3b6faf4
WeaverD9028 <dcw025@latech.edu>
2023-02-27 11:13:48 -0600

    Google Maps API

--- app/src/main/java/com/example/autoaid/GoogleMapsActivity.kt
+++ app/src/main/java/com/example/autoaid/GoogleMapsActivity.kt
@@ -1,8 +1,20 @@
 package com.example.autoaid
 
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,23 +22,152 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.autoaid.databinding.ActivityGoogleMapsBinding
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback,OnMyLocationButtonClickListener,OnRequestPermissionsResultCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityGoogleMapsBinding
+    private lateinit var currentLocation: Location
+    //private val REQUEST_LOCATION_PERMISSION = 1
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var requestPermission: ActivityResultContracts.RequestPermission
         super.onCreate(savedInstanceState)
 
         binding = ActivityGoogleMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getUserCurrentLocation()
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        /*val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Log.i("Permission: ", "Granted")
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    Log.i("Permission: ", "Denied")
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }*/
     }
+    @SuppressLint("MissingPermission")
+    private fun getUserCurrentLocation() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            // Request permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionCode
+            )
+            return
+        }
+            /*if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionCode
+            )
+            return
+        }*/
+
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            // location could be null if the user turns location services even though the app has permission
+            if (location != null) {
+                //stores current location
+                currentLocation = location
+                Toast.makeText(
+                    applicationContext,
+                    currentLocation.latitude.toString() + "" + currentLocation.longitude.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+
+                val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    /*@SuppressLint("MissingPermission")
+    fun requestPermission() {
+        var required_permissions = "Accept or deny"
+        val mySnackbar = Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            required_permissions,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        mySnackbar.show()
+        if (
+            ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            return
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+
+        }
+    }*/
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+           permissionCode -> if(grantResults.contains(PackageManager.PERMISSION_GRANTED))
+               getUserCurrentLocation()
+            }
+        }
+
+
+
+    /*override fun onMyLocationClick(location: Location){
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG)
+            .show()
+    }*/
 
     /**
      * Manipulates the map once available.
@@ -38,11 +179,21 @@ class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(currentLocation.latitude,currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("Current Location")
         mMap = googleMap
-
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7f))
+        mMap.addMarker(markerOptions)
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        /*val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //mMap.setOnMyLocationButtonClickListener(this)
+        //mMap.setOnMyLocationClickListener(this)*/
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
     }
 }
