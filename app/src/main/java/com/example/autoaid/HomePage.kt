e5311b03a625304ece0d6935d156cb75ae00bbcb
WeaverD9028 <dcw025@latech.edu>
2023-04-17 15:46:17 -0500

    Home Page and Sound

--- app/src/main/java/com/example/autoaid/HomePage.kt
+++ app/src/main/java/com/example/autoaid/HomePage.kt
@@ -1,11 +1,30 @@
 package com.example.autoaid
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        val report: Button = findViewById(R.id.btnReport)
+        report.setOnClickListener{
+            startActivity(Intent(this@HomePage,Diagnostic::class.java))
+        }
+
+        val sound: Button  = findViewById(R.id.btnsound)
+        sound.setOnClickListener() {
+            val music: MediaPlayer = MediaPlayer.create(this@HomePage, R.raw.music)
+                music.isLooping = true
+                music.start()
+        }
+
     }
 }
